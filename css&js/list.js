b96b6c11e65a4168fc73e4d6c91ff5c8ce2dcceb
= <=>
2020-01-15 20:01:20 +0700

    first commit

---
+++ css&js/list.js
@@ -0,0 +1,102 @@
+
+window.list=[
+{
+
+	Avatar:"../img/avt.png",
+	Name:"Đào Thị Mận",
+	Birthday:"10/01/1997",
+	Address:"Đà Nẵng",
+	Degree:" Đại học",
+	Detail:"Chi tiết",
+	Skills:[
+			{LanguageName:"Java",Exp:"2"},
+			{LanguageName:"C++",Exp:"3"}			
+			]
+},
+{
+	Avatar:"../img/avt.png",
+	Name:"Đào Thị Bưởi",
+	Birthday:"10/01/1997",
+	Address:"Đà Nẵng",
+	Degree:" Đại học",
+	Detail:"Chi tiết",
+	Skills:[
+			{LanguageName:"Java",Exp:"2"},
+			{LanguageName:"C++",Exp:"3"},
+			{LanguageName:"Php",Exp:"1.5"}
+			]	
+},
+{
+	Avatar:"../img/avt.png",
+	Name:"Lê Mận Cam",
+	Birthday:"10/01/1997",
+	Address:"Đà Nẵng",
+	Degree:" Đại học",
+	Detail:"Chi tiết",
+	Skills:[
+			{LanguageName:"Java",Exp:"2"},
+			{LanguageName:"C++",Exp:"3"},
+			{LanguageName:"C#",Exp:"1"}
+			]	
+},
+{
+	Avatar:"../img/avt.png",
+	Name:"Lê Cam Xoài",
+	Birthday:"10/01/1997",
+	Address:"Đà Nẵng",
+	Degree:" Đại học",
+	Detail:"Chi tiết",
+	Skills:[
+			{LanguageName:"Java",Exp:"2"},
+			{LanguageName:"Android",Exp:"3"}	
+			]	
+},
+{
+	Avatar:"../img/avt.png",
+	Name:"Cam Xoài Ổi",
+	Birthday:"10/01/1997",
+	Address:"Đà Nẵng",
+	Degree:" Đại học",
+	Detail:"Chi tiết",
+	Skills:[
+			{LanguageName:"Java",Exp:"2"},
+			{LanguageName:"C++",Exp:"3"},
+			{LanguageName:"IOS",Exp:"2.5"}
+
+			]		
+},
+{
+	Avatar:"../img/avt.png",
+	Name:"Cam Xoài Quýt",
+	Birthday:"10/01/1997",
+	Address:"Đà Nẵng",
+	Degree:" Đại học",
+	Detail:"Chi tiết",
+	Skills:[
+			{LanguageName:"Java",Exp:"2"},
+			{LanguageName:"C++",Exp:"3"},
+			{LanguageName:".Net",Exp:"3"}
+
+			]
+},
+{
+	Avatar:"../img/avt.png",
+	Name:"Cam Xoài Táo",
+	Birthday:"10/01/1997",
+	Address:"Đà Nẵng",
+	Degree:" Đại học",
+	Detail:"Chi tiết",
+	Skills:[
+			{LanguageName:"Java",Exp:"2"},
+			{LanguageName:"C++",Exp:"3"},
+			{LanguageName:"C#",Exp:"2.5"},
+			{LanguageName:".Net",Exp:"1"}
+			]	
+}
+];
+
+
+
+
+
+
